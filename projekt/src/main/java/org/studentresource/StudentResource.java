8886413f95463cb2ca9fdca104e56b80d9c15fe9
JHeczko <jakub.heczko5@gmail.com>
2023-12-15 12:20:07 +0100

    Setup lab08

--- projekt/src/main/java/org/studentresource/StudentResource.java
+++
@@ -1,9 +0,0 @@
-package org.studentresource;
-
-public interface StudentResource {
-    String getId();
-    void setId(String id);
-    void setName(String name);
-    String getName();
-    // Add other necessary methods here
-}
